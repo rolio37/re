a2076d42a752888c3c442eb9ea8e3907be8b6fe1
Chédotal Julien <rjdi@wanadoo.fr>
2024-10-10 00:10:44 +0200

    formatting

--- crackme_xmm.c
+++ crackme_xmm.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
- float somme=1.2;
- float essai;
+int main () {
 
- fprintf(stdout, "sum = ");
- scanf("%f", &essai);
+float somme=1.2;
+float essai;
+
+ fprintf (stdout, "sum = ");
+ scanf ("%f", &essai);
  
- if (essai == somme) {
-	fprintf(stdout, "good sum !\n");
- } else {
-	fprintf(stdout, "Bad sum !\n");
- }
-
+	if (essai == somme) {
+		fprintf (stdout, "good sum !\n");
+	 } else {
+		fprintf (stdout, "Bad sum !\n");
+ 	}	
 }
