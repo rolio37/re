a49971481062e44d735062d3058f1ce9a21ce07d
Chédotal Julien <rjdi@wanadoo.fr>
2024-10-18 16:26:10 +0200

    Little update

--- crackme_xmm.c
+++ crackme_xmm.c
@@ -2,13 +2,13 @@
 
 int main () {
 
-float somme=1.2;
-float essai;
+double sum=15.95458977019100329811178809273454654654654654654654654654;
+double input;
 
  fprintf (stdout, "sum = ");
- scanf ("%f", &essai);
+ scanf ("%lf", &input);
  
-	if (essai == somme) {
+	if (input == sum) {
 		fprintf (stdout, "good sum !\n");
 	 } else {
 		fprintf (stdout, "Bad sum !\n");
