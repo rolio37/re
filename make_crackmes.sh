8a186a4e6b23808660733c9c32b6f5afa987f7ec
Chédotal Julien <rjdi@wanadoo.fr>
2024-10-27 19:44:15 +0100

    Add crackme_fpu

--- make_crackmes.sh
+++ make_crackmes.sh
@@ -4,5 +4,7 @@ cc crackme_easy.c -o re_crackmes/crackme_easy
 cc crackme_easy2.c -o re_crackmes/crackme_easy2
 cc crackme_easy3.c -o re_crackmes/crackme_easy3
 cc crackme_xmm.c -o re_crackmes/crackme_xmm
-strip re_crackmes/crackme_easy re_crackmes/crackme_easy2 re_crackmes/crackme_easy3 re_crackmes/crackme_xmm 
+cc crackme_xmm.c  -mno-sse -o re_crackmes/crackme_fpu
+strip re_crackmes/crackme_easy re_crackmes/crackme_easy2 re_crackmes/crackme_easy3 re_crackmes/crackme_xmm re_crackmes/crackme_fpu
+
 
